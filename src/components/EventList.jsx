0686ebb11cefb53b39c93d8640bda99a400fbaa4
frndeva <evaferrand33@gmail.com>
2025-10-31 09:32:41 +0100

    feat: SearchFilter

--- src/components/EventList.jsx
+++ src/components/EventList.jsx
@@ -2,13 +2,18 @@ import { Box, Grid } from "@mui/material";
 import { EventCard } from "./EventCard";
 import "../styles/components/event-list.css";
 
-function EventList({ events }) {
+function EventList({ events, search }) {
+
+    const filteredEvents = events.filter((event) =>
+        event.name.toLowerCase().includes(search.toLowerCase())
+    );
+    
     return (
         <Box className="event-list-container">
             <p variant="4" className="event-list-title">Tous les évenements - {events.length}</p>
             {events.length > 0 ? (
                 <Grid container spacing={1}>
-                    {events && events.map((event) => (
+                    {events && filteredEvents.map((event) => (
                         <Grid size={{ xs: 12, sm: 6, md: 4, lg: 3 }}>
                             <EventCard key={event.id} event={event} />
                         </Grid>
--- src/services/api.js
+++ src/services/api.js
@@ -11,13 +11,7 @@ export async function fetchEvents(city = null, category = null) {
     const params = new URLSearchParams();
     if (city) params.append("city", city);
     if (category) params.append("category", category);
-    const url = city && category
-    ? `${API_URL}/events?city=${city}&category=${category}` // ✅ & et pas ?
-    : city
-    ? `${API_URL}/events?city=${city}`
-    : category
-    ? `${API_URL}/events?category=${category}`
-    : `${API_URL}/events`;
+    const url = `${API_URL}/events${params.toString() ? `?${params}` : ""}`;
     const response = await fetch(url);
     const data = await response.json();
     return data.data;
